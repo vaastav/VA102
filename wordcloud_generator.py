ae4c38e43edeb1c31d1de7f63c45da99af3f0e5c
vaastav <vaastav.anand05@gmail.com>
2016-03-14 22:52:37 -0700

    Add script to generate simple wordclouds

---
+++ wordcloud_generator.py
@@ -0,0 +1,15 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import csv
+
+text = ""
+with open('cleaned_items.csv','rU') as reviews:
+	reader = csv.DictReader(reviews)
+	for row in reader:
+		text += row['Cleaned_Review'] + " "
+
+wordcloud = WordCloud(max_font_size=40,relative_scaling=.5).generate(text)
+plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
